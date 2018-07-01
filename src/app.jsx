e73d9f8fdead595886dcfa7bb8addc514b037d81
ze25800000 <ze258100000@sina.com>
2018-07-01 13:35:53 +0800

    6-5 侧边导航的开发

--- src/app.jsx
+++ src/app.jsx
@@ -13,7 +13,8 @@ class App extends Component {
                 <Layout>
                     <Switch>
                         <Route exact path='/' component={Home}/>
-                        <Redirect from={"*"} to={'/'}/>
+                        <Route exact path='/product' component={Home}/>
+                        <Route exact path='/product.category' component={Home}/>
                     </Switch>
                 </Layout>
             </Router>
